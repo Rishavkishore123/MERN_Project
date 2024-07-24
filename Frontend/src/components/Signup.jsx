eb4e870821db5ce6ba0b0bebee18ff16cd928f95
RISHAV KISHORE <rishavkishore732@gmail.com>
2024-07-25 00:36:46 +0530

    Updated_Signup

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -6,6 +6,7 @@ const Signup = () => {
     const[name, setName]=useState('');
     const [email,setEmail]=useState('');
     const [password, setPassword]= useState('');
+    const [error, setError]= useState(false);
     const navigate= useNavigate();
 
     useEffect(()=>{
@@ -17,9 +18,10 @@ const Signup = () => {
     
 
     const collectData=async()=>{
-
-
-      
+      if(!name || !email || !password){
+        setError(true);
+        return false;
+      }
       let result= await fetch('http://localhost:3001/signup',{
         method:"post",
         body: JSON.stringify({name,email,password}),
@@ -46,10 +48,13 @@ const Signup = () => {
         <div className="card-body  ">
         <h5 className="card-title">Name</h5>
         <input type='text 'className='px-4'value={name} onChange={(e)=>setName(e.target.value)} placeholder='enter your name'/>
+        {error && !name && <span className='text-danger'>Please Fill  the boxes</span>}
         <h5 className="card-title my-2 ">E-mail</h5>
         <input type='text' className='px-4 ' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='enter your e-mail'/>
+        {error && !email && <span className='text-danger'>please fill e-mail box</span>}
         <h5 className="card-title  my-2">Password</h5>
         <input type='password'className='px-4' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password'/>
+        {error && !password && <span className='text-danger'>please fill password box</span>}
        </div>
       </div>
 
